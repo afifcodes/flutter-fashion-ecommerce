10fb70e4cab8e41371f9b59c16fd6e5f238673b2
afifudinx <work.afifudin@gmail.com>
2023-01-04 17:12:22 +0700

    Configure runApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_fashion_ecommerce/screens/detail_screen.dart';
 import 'package:flutter_fashion_ecommerce/screens/home_screen.dart';
+import 'package:flutter_fashion_ecommerce/utils/route_animation.dart';
+import 'package:flutter_fashion_ecommerce/utils/route_arguments.dart';
 
 void main() => runApp(MaterialApp(
       theme: ThemeData(fontFamily: 'Manrope'),
       debugShowCheckedModeBanner: false,
-      home: HomeScreen(),
+      initialRoute: '/',
+      onGenerateRoute: (settings) {
+        final arguments = settings.arguments;
+        if (settings.name == '/detail') {
+          DetailScreenArguments args = arguments as DetailScreenArguments;
+          return RouteAnimation.slide(
+              settings,
+              DetailScreen(
+                  name: args.name,
+                  price: args.price,
+                  is_liked: args.is_liked,
+                  image_url: args.image_url));
+        } else {
+          return RouteAnimation.slide(settings, const HomeScreen());
+        }
+      },
     ));
